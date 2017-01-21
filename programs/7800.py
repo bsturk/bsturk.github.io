60682b66f901b4a6772f50b80ffe9c90d9f7ccf4
bsturk <bsturk@comcast.net>
2017-01-21 15:51:53 -0500

    more updates to frontend stuff

---
+++ programs/7800.py
@@ -0,0 +1,34 @@
+import os, socket
+
+title         = 'Atari 7800'
+has_submenus  = True
+app_name      = r'g:\rec\emu\multi\mess\mess.exe'
+app_args      = r'a7800 -video ddraw -waitvsync -rompath g:\rec\emu\multi\mess\bios -cfg_directory g:\rec\emu\multi\mess\cfg -joystick -cart "%F"'
+rom_dirs      = r'g:\rec\emu\atari\7800\media\cart\games'
+rom_exts      = 'a78;rom'
+pic_dirs      = r'g:\rec\emu\atari\7800\pic\games'
+pre_cmd       = r'g:\dev\proj\frontend\pygame\joykey.bat'
+pre_cmd_args  = r'7800.cfg'
+post_cmd      = r'g:\dev\proj\frontend\pygame\joykey.bat'
+post_cmd_args = r'menu.cfg'
+pic           = r'pics/7800.jpg'
+info          = 'Select -> S      Reset -> R;Pause -> P     Exit -> Esc or hold Fire for 3+ seconds'
+
+if os.name == 'posix':
+    home_dir = os.path.expanduser( '~' )
+    pic_dirs = home_dir + r'/rec/emu/atari/7800/pic/games'
+    rom_dirs = home_dir + r'/rec/emu/atari/7800/media/cart/games'
+
+## includes light gun and paddle games
+ignores       = [
+    'asteroids_unencrypted.a78',
+    'alien_brigade.a78',
+    'barnyard_blaster.a78',
+    'crossbow.a78',
+    'meltdown.a78',
+    'sentinel.a78',
+    'crazy_bricks.a78',
+    'ms_pac_man-320.a78',
+    'ms_pac_man-320-pokey.a78',
+    'rampart.a78',			##  not complete prototype
+]
--- programs/ti99.py
+++ programs/ti99.py
@@ -1,23 +1,53 @@
-mess_args    = 'ti99_4a -now -nonu'            ##  not a mandatory entry, just useful since it could be re-used a lot
+import os, string, socket
 
-title        = 'Ti-99/4A'
-has_submenus = True
-app_name     = r'g:\rec\emu\misc\mess\mess.exe'
-app_args     = mess_args + ' -cart "%F"'
-rom_dirs     = r'g:\rec\emu\ti\ti99_4a\dsk\games\;g:\rec\emu\ti\ti99_4a\dsk\games\0_9;g:\rec\emu\ti\ti99_4a\dsk\games\a;g:\rec\emu\ti\ti99_4a\dsk\games\b;g:\rec\emu\ti\ti99_4a\dsk\games\c;g:\rec\emu\ti\ti99_4a\dsk\games\d;g:\rec\emu\ti\ti99_4a\dsk\games\e;g:\rec\emu\ti\ti99_4a\dsk\games\f;g:\rec\emu\ti\ti99_4a\dsk\games\g;g:\rec\emu\ti\ti99_4a\dsk\games\h;g:\rec\emu\ti\ti99_4a\dsk\games\i;g:\rec\emu\ti\ti99_4a\dsk\games\j;g:\rec\emu\ti\ti99_4a\dsk\games\k;g:\rec\emu\ti\ti99_4a\dsk\games\l;g:\rec\emu\ti\ti99_4a\dsk\games\m;g:\rec\emu\ti\ti99_4a\dsk\games\n;g:\rec\emu\ti\ti99_4a\dsk\games\o;g:\rec\emu\ti\ti99_4a\dsk\games\p;g:\rec\emu\ti\ti99_4a\dsk\games\q;g:\rec\emu\ti\ti99_4a\dsk\games\r;g:\rec\emu\ti\ti99_4a\dsk\games\s;g:\rec\emu\ti\ti99_4a\dsk\games\t;g:\rec\emu\ti\ti99_4a\dsk\games\u;g:\rec\emu\ti\ti99_4a\dsk\games\v;g:\rec\emu\ti\ti99_4a\dsk\games\w;g:\rec\emu\ti\ti99_4a\dsk\games\x;g:\rec\emu\ti\ti99_4a\dsk\games\y;g:\rec\emu\ti\ti99_4a\dsk\games\z'
-rom_exts     = 'bin;dsk'
-pic_dir      = r'g:\rec\emu\ti\ti99_4a\pic\games'
-pre_cmd      = r'g:\rec\emu\misc\utils\joytokey\joykey.bat'
-pre_cmd_args = 'ti99.cfg'
-post_cmd     = r'g:\rec\emu\misc\utils\joytokey\joykey.bat'
-post_cmd_args= 'menu.cfg'
-pic          = r'pics\ti99.jpg'
-info         = 'Texas Instruments 99/4A'
+mess_args     = r'ti99_4a -video ddraw -joystick -rompath g:\rec\emu\multi\mess\bios -cfg_directory g:\rec\emu\multi\mess\cfg'
+xbas_args     = r'-cartridge g:\rec\emu\ti\ti99_4a\media\cart\dev\extended_basic.rpk'
+disk_args     = r'-cartridge g:\rec\emu\ti\ti99_4a\media\cart\util\disk_manager_1.rpk'
 
-overrides    = { 
-    'parsec_c.bin' : ( 'parsec', 'DEFAULT', mess_args + r' -cart %D\parsec_g.bin -cart %D\parsec_c.bin',  'DEFAULT',  'DEFAULT',   'DEFAULT',  'DEFAULT',  'DEFAULT' ),
+title         = 'TI 99/4A'
+has_submenus  = True
+app_name      = r'g:\rec\emu\multi\mess\mess.exe'
+app_args      = mess_args + ' -cart1 "%F"'
+rom_dirs      = ''
+pic_dirs      = ''
+rom_exts      = 'rpk;dsk'
+pre_cmd       = r'g:\dev\proj\frontend\pygame\joykey.bat'
+pre_cmd_args  = 'ti99.cfg'
+post_cmd      = r'g:\dev\proj\frontend\pygame\joykey.bat'
+post_cmd_args = 'menu.cfg'
+pic           = r'pics/ti99.jpg'
+info          = 'FCTN key -> Alt   Redo -> FCTN & 8   Back -> FCTN & 9;Exit -> F6 then Esc or hold Fire for 3+ seconds'
+
+rom_root      = r'g:\rec\emu\ti\ti99_4a\media\cart\games'
+dsk_root      = r'g:\rec\emu\ti\ti99_4a\media\dsk\games'
+pic_root      = r'g:\rec\emu\ti\ti99_4a\pic\games'
+
+if os.name == 'posix':
+    home_dir = os.path.expanduser( '~' )
+    rom_root = home_dir + r'/rec/emu/ti/ti99_4a/media/cart/games'
+    dsk_root = home_dir + r'/rec/emu/ti/ti99_4a/media/dsk/games'
+    pic_root = home_dir + r'/rec/emu/ti/ti99_4a/pic/games'
+
+for letter in string.lowercase:
+    rom_dirs += rom_root + os.sep + letter + ';'
+    rom_dirs += dsk_root + os.sep + letter + ';'
+    pic_dirs += pic_root + os.sep + letter + ';'
+
+rom_dirs += rom_root + os.sep + '0_9' + ';'
+rom_dirs += dsk_root + os.sep + '0_9'
+pic_dirs += pic_root + os.sep + '0_9'
+
+overrides     = { 
+        #'.+\.dsk' : ( 'DEFAULT', 'DEFAULT', mess_args + r' -peb:slot7 hfdc -floppydisk1 "%F" ' + xbas_args,  'DEFAULT',  'DEFAULT',   'DEFAULT',  'DEFAULT',  'DEFAULT' ),
+        '.+\.dsk' : ( 'DEFAULT', 'DEFAULT', mess_args + r' -peb:slot7 hfdc -floppydisk1 "%F" ' + disk_args,  'DEFAULT',  'DEFAULT',   'DEFAULT',  'DEFAULT',  'DEFAULT' ),
 }
 
-ignores      = [
-    'parsec_g.bin'
+filters       = [
+    '.*-alt.*.rpk',
+    '.*-alt.*.dsk',
+    '.*-beta.*.rpk',
+]
+
+ignores       = [
+    'tunnels_of_doom_quest_or_pennies.dsk',
 ]
